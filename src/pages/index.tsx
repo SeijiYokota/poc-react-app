009c90a333ba3859d5eeb110e28c034109010fce
横田 靖司 <130006310+SeijiYokota@users.noreply.github.com>
2024-04-18 10:51:46 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <main className={`flex min-h-screen flex-col items-center p-24`}>
-      <h1>🤖なんでも聞いてください🤖</h1>
+      <h1>なんでも聞いてください</h1>
 
       <label
         htmlFor="message"
